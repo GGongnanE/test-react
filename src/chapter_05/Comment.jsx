bc8fc3f58c9dfbc15f39ae6bc0546afbe29971a9
GGongnanE <z_zzlzl@naver.com>
2025-02-19 15:55:42 +0900

    Add sample code

---
+++ src/chapter_05/Comment.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+// const styles = {
+//     wrapper: {
+//         margin: 8,
+//         padding: 8,
+//         display: 'flex',
+//         flexDirection: 'row',
+//         border: '1px solid grey',
+//         borderRadius: 16,
+//     },
+//     imageContainer: {},
+//     image: {
+//         width: 100,
+//         height: 100,
+//         borderRadius: 25,
+//     },
+//     contentContainer: {
+//         marginLeft: 8,
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//     },
+//     nameText: {
+//         fontSize: 20,
+//         fontWeight: 'bold',
+//         color: 'black'
+//     },
+//     commentText: {
+//         fontSize: 16,
+//         color: 'darkgrey',
+//         fontWeight: 'bold',
+//     }
+// }
+
+function Comment(props) {
+    return (
+        <div>
+            <h1>첫 번째 컴포넌트</h1>
+        </div>
+    );
+}
+
+export default Comment;
