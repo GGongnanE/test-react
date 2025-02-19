71b301154ac52bee87826022c1bb624f1d07bda4
GGongnanE <z_zzlzl@naver.com>
2025-02-19 16:01:35 +0900

    Add css code

--- src/chapter_05/Comment.jsx
+++ src/chapter_05/Comment.jsx
@@ -1,42 +1,53 @@
 import React from "react";
 
-// const styles = {
-//     wrapper: {
-//         margin: 8,
-//         padding: 8,
-//         display: 'flex',
-//         flexDirection: 'row',
-//         border: '1px solid grey',
-//         borderRadius: 16,
-//     },
-//     imageContainer: {},
-//     image: {
-//         width: 100,
-//         height: 100,
-//         borderRadius: 25,
-//     },
-//     contentContainer: {
-//         marginLeft: 8,
-//         display: 'flex',
-//         flexDirection: 'column',
-//         justifyContent: 'center',
-//     },
-//     nameText: {
-//         fontSize: 20,
-//         fontWeight: 'bold',
-//         color: 'black'
-//     },
-//     commentText: {
-//         fontSize: 16,
-//         color: 'darkgrey',
-//         fontWeight: 'bold',
-//     }
-// }
+const styles = {
+    wrapper: {
+        margin: 8,
+        padding: 8,
+        display: 'flex',
+        flexDirection: 'row',
+        border: '1px solid grey',
+        borderRadius: 16,
+    },
+    imageContainer: {},
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 25,
+    },
+    contentContainer: {
+        marginLeft: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    nameText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'black'
+    },
+    commentText: {
+        fontSize: 16,
+        color: 'darkgrey',
+        fontWeight: 'bold',
+    }
+}
 
 function Comment(props) {
     return (
-        <div>
-            <h1>첫 번째 컴포넌트</h1>
+        <div style={styles.wrapper}>
+            <div style={styles.imageContainer}>
+                <img 
+                    style={styles.image} 
+                    src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+                />
+            </div>
+            <div style={styles.contentContainer}>
+                <span style={styles.nameText}>김길숙</span>
+                <span style={styles.commentText}>
+                    This is first comment...
+                </span>
+            </div>
         </div>
     );
 }
