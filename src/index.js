e03df1797a310ba429ee56272f6b80cd7be941bd
GGongnanE <z_zzlzl@naver.com>
2025-02-20 09:48:53 +0900

    Rename chapter03

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from './ chapter_03/Library';
+import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 
